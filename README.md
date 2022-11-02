fe619ae2373ec91a642d167db9537b29fab9ea30
azolyomi <64488367+azolyomi@users.noreply.github.com>
2022-11-01 21:20:12 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Name -- Building a <Name> using React.js 
+
+### Usage
+Type `npm start` in the directory of the project to get it running.
+The browser should open a new tab at `localhost:3000`. If it doesn't open this tab manually.
---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+// This file contains the logic for rendering your primary components.
+// Rendering a subcomponent of a component A should be done in the logic of A.js, not here.
+
+import * as React from 'react';
+
+function App() {
+  return (
+    <div>hello</div>
+  );
+}
+
+export default App;
